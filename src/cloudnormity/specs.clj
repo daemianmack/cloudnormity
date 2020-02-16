7e4fe792244220e3a02564e8bcf4c66345e845ed
Daemian Mack <daemianmack@gmail.com>
2020-02-16 09:29:47 -0500

    Rehome responsibility of managing tx-source values

--- src/cloudnormity/specs.clj
+++ src/cloudnormity/specs.clj
@@ -11,35 +11,13 @@
 (s/def ::mutable boolean?)
 (s/def ::name keyword?)
 
-(defn distinguish-tx-sources
-  "If a key in the `norm-map` has a method implementation in
-  `tx-data-for-norm`, relocate it underneath a `:tx-source` key with a
-  vector conveying its type and contents.
-
-  This allows the user's config to reference novel tx sources while
-  keeping general-usage syntax as simple and terse as possible (i.e.,
-  user does not have to provide a `type` key or worry about special
-  structure) yet also give `tx-data-for-norm` something at a known
-  coordinate to dispatch on."
-  [norm-map]
-  (let [meths (methods tx-sources/tx-data-for-norm)]
-    (reduce-kv
-     (fn [acc k v]
-       (if (contains? meths k)
-         (reduced (-> acc
-                      (assoc :tx-source [k v])
-                      (dissoc k)))
-         acc))
-     norm-map
-     norm-map)))
-
 (s/def ::norm-map
   (s/and (s/keys :req-un [::name]
                  :opt-un [::tx-data
                           ::tx-resource
                           ::tx-fn
                           ::mutable])
-         (s/conformer distinguish-tx-sources)))
+         (s/conformer tx-sources/reorg-tx-sources)))
 
 (s/def ::norm-maps
   (s/coll-of ::norm-map))
--- src/cloudnormity/tx_sources.clj
+++ src/cloudnormity/tx_sources.clj
@@ -26,3 +26,26 @@
 (defmethod tx-data-for-norm :tx-fn tx-fn
   [conn norm-map]
   (->> norm-map :tx-source second (eval-tx-fn conn)))
+
+
+(defn reorg-tx-sources
+  "If a key in the `norm-map` has a method implementation in
+  `tx-data-for-norm`, relocate it underneath a `:tx-source` key with a
+  vector conveying its type and value.
+
+  This allows the user's config to reference novel tx sources while
+  keeping config syntax as simple and terse as possible (i.e., user
+  does not have to provide an extraneous `type` key, or preserve
+  special structure) yet also gives `tx-data-for-norm` a dispatchable
+  value at a predictable coordinate."
+  [norm-map]
+  (let [meths (methods tx-data-for-norm)]
+    (reduce-kv
+     (fn [acc k v]
+       (if (contains? meths k)
+         (reduced (-> acc
+                      (assoc :tx-source [k v])
+                      (dissoc k)))
+         acc))
+     norm-map
+     norm-map)))
